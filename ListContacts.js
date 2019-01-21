04c8ff2189b12e1c5f40c2950e962e2b0d4b051f
emilyjf <flanagan.emily.j@gmail.com>
2019-01-21 15:09:38 -0600

    Added name and handle to contacts

--- ListContacts.js
+++ ListContacts.js
@@ -7,8 +7,17 @@ class ListContacts extends Component {
         return (
             <ol className='contact-list'>
                 {this.props.contacts.map((contact) => (
-                    <li key={contact.id}>
-                        {contact.name}
+                    <li key={contact.id} className='contact-list-item'>
+                        <div 
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>    
+                        </div>
                     </li>
                 ))}
             </ol>
