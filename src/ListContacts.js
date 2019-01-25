65a73c6a71afce66805e6678ca85d71f18be3468
emilyjf <flanagan.emily.j@gmail.com>
2019-01-25 10:35:29 -0600

    Refactored code; no longer relies on "this" keyword

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
-
-class ListContacts extends Component {
-    render() {
-        //to see what data we're passing in as props
-        console.log('Props', this.props)
-        return (
-            <ol className='contact-list'>
-            
-            </ol>
-        )
-    }
+import React from 'react'
+//import PropTypes from 'prop-types'
+//import { Link } from 'react-router-dom'
+function ListContacts () {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div 
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>    
+                    </div>
+                    <button className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
 
+
 export default ListContacts
+
