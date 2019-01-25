88dc14e312283c9666ed01e862cf0146791f71be
emilyjf <flanagan.emily.j@gmail.com>
2019-01-25 13:00:55 -0600

    Updated to included onClick to delete contact

--- ListContacts.js
+++ ListContacts.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
-
-class ListContacts extends Component {
-    render() {
-        //to see what data we're passing in as props
-       
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div 
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>    
-                        </div>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+import React from 'react'
+//import PropTypes from 'prop-types'
+//import { Link } from 'react-router-dom'
+function ListContacts () {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div 
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>    
+                    </div>
+                    <button 
+                        onClick={() => props.onDeleteContact(contact)}
+                        className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
+
 
 export default ListContacts
